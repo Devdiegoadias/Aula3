0a3e52030b69fd2c1a2766ea1428f961f665864d
Diego A. Dias <dtdias@icatuseguros.com.br>
2021-10-14 10:11:18 -0300

    .

--- Aula3_OperadoresLogicos/Program.cs
+++ Aula3_OperadoresLogicos/Program.cs
@@ -6,7 +6,31 @@ namespace OperadoresLogicos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Operador ! (NEGAÇÃO)
+            bool variavel = false;
+            Console.WriteLine(!variavel); //Retorna True
+            Console.WriteLine(!true);     //Retorna False
+
+            //Operador && (AND)
+            int idade;
+            bool idadeValida;
+
+            Console.WriteLine("Digita sua idade:");
+            idade = int.Parse(Console.ReadLine());
+
+            idadeValida = idade >= 0 && idade <= 120;
+
+            Console.WriteLine(idadeValida.ToString());
+
+            //Operador || (OU)
+            int temperatura;
+            bool temperaturaInvalida;
+
+            Console.WriteLine("Qual temperatura a água está em estado líquido?");
+            temperatura = int.Parse(Console.ReadLine());
+            temperaturaInvalida = (temperatura < 0) || (temperatura > 100);
+
+            Console.WriteLine(temperaturaInvalida.ToString());
         }
     }
 }
